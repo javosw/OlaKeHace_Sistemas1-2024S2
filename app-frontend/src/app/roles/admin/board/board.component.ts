39e92590ef55dc16425c988b1498588d3973e0d8
Javier Oswaldo <javoswsq@gmail.com>
2024-10-02 12:39:19 -0600

    cambios irrelvantes

---
+++ app-frontend/src/app/roles/admin/board/board.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'okh-board',
+  standalone: true,
+  imports: [],
+  templateUrl: './board.component.html',
+})
+export class BoardComponent {
+
+}
